5ddc4bd0685e733f1c80d9902a1be9a3abda07cd
Exequiel Zalazar <exequiel.zalazar@nonerds.com>
2024-02-25 09:52:39 -0300

    listo back

--- back/task/api.py
+++ back/task/api.py
@@ -1,5 +1,7 @@
 
-from rest_framework import viewsets, permissions
+from rest_framework import viewsets, permissions, status
+from rest_framework.decorators import action
+from rest_framework.response import Response
 from .models import Task
 from .serializador import TaskS
 # Create your views here.
@@ -9,3 +11,9 @@ class TaskView(viewsets.ModelViewSet):
     queryset = Task.objects.all()
     permisos = [permissions.AllowAny]
     serializer_class = TaskS
+    @action(detail=True, methods=['POST'])
+    def done(self, pk=None):
+        task=self.get_object()
+        task.done = not task.done
+        task.save()
+        return Response({'status':'tarea marcada como hecha' if task.done else 'tarea marcada como no hecha'},status=status.HTTP_200_OK)
