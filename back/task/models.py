49e594494eff975cfa718c3a4ed52e9e1ca74079
Exequiel Zalazar <exequiel.zalazar@nonerds.com>
2024-02-24 21:19:53 -0300

    inicio del proyecto

---
+++ back/task/models.py
@@ -0,0 +1,7 @@
+from django.db import models
+
+class User(models.Model):
+    title = models.CharField(max_length=50)
+    description = models.TextField
+    done=models.BooleanField(default=False)
+    creat_at = models.DateTimeField(auto_now_add=True)
